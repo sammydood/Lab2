6ff51777fe5d514e88cfe6603d18ec6f1d511abc
Samuel Tan <sammydood123@gmail.com>
2025-05-03 03:08:33 +0800

    editted Lab2

--- lab2/Lab2.py
+++ lab2/Lab2.py
@@ -1,5 +1,9 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list=get_user_input()
 
+&
 def funcName(parameter1, parameter2):
     print("this is a dummy function")
     return 10
