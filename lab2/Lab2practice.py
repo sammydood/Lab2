c5d29dab6eed79d9f2ee4993f68a30243df2d56c
Samuel Tan <sammydood123@gmail.com>
2025-05-03 04:24:44 +0800

    Temperature Calculator

--- lab2/Lab2practice.py
+++ lab2/Lab2practice.py
@@ -8,26 +8,31 @@ def main():
     median = calc_median_temperature(num_list)
 
     print(f"\nResults:")
-    print(f"Average Temperature: {average:.2f}")
+    print(f"Average Temperature:{average:.2f}")
     print(f"Minimum and Maximum Temperatures: {min_max}")
     print(f"Sorted Temperatures: {sorted_list}")
-    print(f"Median Temperature: {median:.2f}")
+    print(f"Median Temperatures: {median:.2f} ")
+
 
 def display_main_menu():
     print("Main Menu")
-    print("1. Enter temperatures")
-    print("2. Show results")
+    print("1. Enter Temperature")
+    print("2. Show Results")
     print("3. Exit")
 
+
+
 def get_user_input():
-    print("Enter temperatures separated by commas (e.g. 22.5,24,19.8):")
+    print("Enter temperatures seperated by commas (e.g. 20.3, 343.46, 22.9): ")
     input_str = input()
     temp_list = [float(temp.strip()) for temp in input_str.split(",") if temp.strip()]
     return temp_list
 
+
 def calc_average(temperatures):
     print("Calculating average...")
     return sum(temperatures) / len(temperatures)
+
 
 def find_min_max(temperatures):
     print("Finding min and max...")
@@ -37,11 +42,12 @@ def sort_temperature(temperatures):
     print("Sorting temperatures...")
     return sorted(temperatures)
 
+
 def calc_median_temperature(temperatures):
     print("Calculating median...")
     sorted_temps = sorted(temperatures)
     n = len(sorted_temps)
-    mid = n // 2
+    mid = n//2
     if n % 2 == 0:
         return (sorted_temps[mid - 1] + sorted_temps[mid]) / 2
     else:
