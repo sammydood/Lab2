c19173ee2a3f75a43702212b6fb8d81b66cec756
Samuel Tan <sammydood123@gmail.com>
2025-05-09 00:43:27 +0800

    edited bmi.py

--- lab2/bmi.py
+++ lab2/bmi.py
@@ -12,10 +12,13 @@ def calculate_bmi(height, weight):
     #Part (d)
     if(bmi < 18.5):
         print("Under Weight")
+        return -1
     elif(18.5 <= bmi <= 25.0):
         print("Normal Weight")
+        return 0
     else:
         print("Over Weight")
+        return 1
 
-
-calculate_bmi(weight=57,height=1.73)
+result = calculate_bmi(weight=57,height=1.73)
+print("Return Value", result)
